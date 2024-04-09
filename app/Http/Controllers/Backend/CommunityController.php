fc58bbb59ad916767d7e84f3b2d070d4967fce89
ekiidx <info@vuedesign.co>
2024-04-09 15:08:06 -0400

    removed resource methods from CommunityController

--- app/Http/Controllers/Backend/CommunityController.php
+++ app/Http/Controllers/Backend/CommunityController.php
@@ -48,8 +48,10 @@ class CommunityController extends Controller
      */
     public function store(CommunityStoreRequest $request)
     {
-        Community::create($request->validated() + ['user_id' => auth()->id()]);
-        return to_route('communities.index')->with('message', 'Community created successfully.');
+        // Community::create($request->validated() + ['user_id' => auth()->id()]);
+        // return to_route('communities.index')->with('message', 'Community created successfully.');
+
+        return to_route('communities.index');
     }
 
     /**
@@ -60,7 +62,7 @@ class CommunityController extends Controller
      */
     public function show($id)
     {
-        //
+        return to_route('communities.index');
     }
 
     /**
@@ -71,8 +73,10 @@ class CommunityController extends Controller
      */
     public function edit(Community $community)
     {
-        $this->authorize('update', $community);
-        return Inertia::render('Communities/Edit', compact('community'));
+        // $this->authorize('update', $community);
+        // return Inertia::render('Communities/Edit', compact('community'));
+
+        return to_route('communities.index');
     }
 
     /**
@@ -84,10 +88,12 @@ class CommunityController extends Controller
      */
     public function update(CommunityStoreRequest $request, Community $community)
     {
-        $this->authorize('update', $community);
-        $community->update($request->validated());
+        // $this->authorize('update', $community);
+        // $community->update($request->validated());
 
-        return to_route('communities.index')->with('message', 'Community updated successfully.');
+        // return to_route('communities.index')->with('message', 'Community updated successfully.');
+
+        return to_route('communities.index');
     }
 
     /**
@@ -98,8 +104,10 @@ class CommunityController extends Controller
      */
     public function destroy(Community $community)
     {
-        $this->authorize('delete', $community);
-        $community->delete();
-        return back()->with('message', 'Community deleted successfully.');
+        // $this->authorize('delete', $community);
+        // $community->delete();
+        // return back()->with('message', 'Community deleted successfully.');
+
+        return to_route('communities.index');
     }
 }
