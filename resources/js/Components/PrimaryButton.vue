327d51f50fc61b674f9f28fc708586204325abcf
ekiidx <info@vuedesign.co>
2024-04-19 17:38:30 -0400

    updated login button style

--- resources/js/Components/PrimaryButton.vue
+++ resources/js/Components/PrimaryButton.vue
@@ -8,7 +8,7 @@ defineProps({
 </script>
 
 <template>
-    <button :type="type" class="items-center border-transparent bg-darkorchid fw-500 text-white font-xsss p-3 text-center lh-20 rounded-xl">
+    <button :type="type" class="primary-btn items-center border-transparent bg-darkorchid fw-600 text-white font-xsss text-center lh-20 rounded-xl">
         <slot />
     </button>
 </template>
