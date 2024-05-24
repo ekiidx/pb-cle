7a560cb4b5a432afd3369159ab652f82a849e260
ekiidx <info@vuedesign.co>
2024-05-23 20:50:53 -0400

    added main site style to auth pages

--- resources/js/Pages/Auth/ConfirmPassword.vue
+++ resources/js/Pages/Auth/ConfirmPassword.vue
@@ -32,32 +32,42 @@ const submit = () => {
 
         <AuthenticationCard>
 
-            <div class="mb-4 text-sm text-gray-600 dark:text-gray-400">
-                This is a secure area of the application. Please confirm your password before continuing.
+            <div class="row">
+
+                <div class="col-xl-5 d-none d-xl-block p-0 vh-100 bg-image-cover bg-no-repeat bg-black" style="background-image: url('../assets/img/3.jpg'); background-size: cover; background-position: center;"></div>
+
+                <div class="col-xl-7 vh-100 align-items-center d-flex bg-black overflow-hidden">
+                    <div class="card bg-black shadow-none border-0 ms-auto me-auto login-card">
+                        <div class="card-body bg-black rounded-0 text-left">
+
+                            <h2 class="text-white fw-700">This is a secure area of the application. Please confirm your password before continuing.</h2>
+                        
+                            <form @submit.prevent="submit">
+                                <div>
+                                    <InputLabel for="password" value="Password" />
+                                    <TextInput
+                                        id="password"
+                                        ref="passwordInput"
+                                        v-model="form.password"
+                                        type="password"
+                                        class="mt-1 block w-full"
+                                        required
+                                        autocomplete="current-password"
+                                        autofocus
+                                    />
+                                    <InputError class="mt-2" :message="form.errors.password" />
+                                </div>
+
+                                <div class="flex justify-end mt-4">
+                                    <PrimaryButton class="ml-4" :class="{ 'opacity-25': form.processing }" :disabled="form.processing">
+                                        Confirm
+                                    </PrimaryButton>
+                                </div>
+                            </form>
+                        </div>
+                    </div>
+                </div>
             </div>
-
-            <form @submit.prevent="submit">
-                <div>
-                    <InputLabel for="password" value="Password" />
-                    <TextInput
-                        id="password"
-                        ref="passwordInput"
-                        v-model="form.password"
-                        type="password"
-                        class="mt-1 block w-full"
-                        required
-                        autocomplete="current-password"
-                        autofocus
-                    />
-                    <InputError class="mt-2" :message="form.errors.password" />
-                </div>
-
-                <div class="flex justify-end mt-4">
-                    <PrimaryButton class="ml-4" :class="{ 'opacity-25': form.processing }" :disabled="form.processing">
-                        Confirm
-                    </PrimaryButton>
-                </div>
-            </form>
         </AuthenticationCard>
     </Guest>
 </template>
--- resources/js/Pages/Auth/ResetPassword.vue
+++ resources/js/Pages/Auth/ResetPassword.vue
@@ -33,54 +33,77 @@ const submit = () => {
 
         <AuthenticationCard>
 
-            <form @submit.prevent="submit">
+            <div class="row">
 
-                <div class="block">
-                    <!-- <InputLabel for="email" value="Email" /> -->
-                    <TextInput
-                        id="email"
-                        v-model="form.email"
-                        type="email"
-                        class="block mt-1 w-full"
-                        required
-                        autofocus
-                        autocomplete="username"
-                    />
-                    <InputError class="mt-2" :message="form.errors.email" />
-                </div>
+                <div class="col-xl-5 d-none d-xl-block p-0 vh-100 bg-image-cover bg-no-repeat bg-black" style="background-image: url('../assets/img/3.jpg'); background-size: cover; background-position: center;"></div>
 
-                <div class="mt-4">
-                    <!-- <InputLabel for="password" value="Password" /> -->
-                    <TextInput
-                        id="password"
-                        v-model="form.password"
-                        type="password"
-                        class="block mt-1 w-full"
-                        required
-                        autocomplete="new-password"
-                    />
-                    <InputError class="mt-2" :message="form.errors.password" />
-                </div>
+                    <div class="col-xl-7 vh-100 align-items-center d-flex bg-black overflow-hidden">
+                        <div class="card bg-black shadow-none border-0 ms-auto me-auto login-card">
+                            <div class="card-body bg-black rounded-0 text-left">
 
-                <div class="mt-4">
-                    <!-- <InputLabel for="password_confirmation" value="Confirm Password" /> -->
-                    <TextInput
-                        id="password_confirmation"
-                        v-model="form.password_confirmation"
-                        type="password"
-                        class="block mt-1 w-full"
-                        required
-                        autocomplete="new-password"
-                    />
-                    <InputError class="mt-2" :message="form.errors.password_confirmation" />
-                </div>
+                                <h2 class="text-white fw-700 display1-size display2-md-size mb-3">Password Reset</h2>
 
-                <div class="flex items-center justify-end mt-4">
-                    <PrimaryButton :class="{ 'opacity-25': form.processing }" :disabled="form.processing">
-                        Reset Password
-                    </PrimaryButton>
+                                <div class="mb-4 text-sm text-gray-600 dark:text-gray-400">
+                                Create a new password and confirm it.
+                            </div>
+
+                            <div v-if="status" class="mb-4 font-medium text-sm text-green-600 dark:text-green-400">
+                                {{ status }}
+                            </div>
+
+
+                            <form @submit.prevent="submit">
+
+                                <div class="block">
+                                    <!-- <InputLabel for="email" value="Email" /> -->
+                                    <TextInput
+                                        id="email"
+                                        v-model="form.email"
+                                        type="email"
+                                        class="block mt-1 w-full"
+                                        required
+                                        autofocus
+                                        autocomplete="username"
+                                    />
+                                    <InputError class="mt-2" :message="form.errors.email" />
+                                </div>
+
+                                <div class="mt-4">
+                                    <!-- <InputLabel for="password" value="Password" /> -->
+                                    <TextInput
+                                        id="password"
+                                        v-model="form.password"
+                                        type="password"
+                                        class="block mt-1 w-full"
+                                        required
+                                        autocomplete="new-password"
+                                    />
+                                    <InputError class="mt-2" :message="form.errors.password" />
+                                </div>
+
+                                <div class="mt-4">
+                                    <!-- <InputLabel for="password_confirmation" value="Confirm Password" /> -->
+                                    <TextInput
+                                        id="password_confirmation"
+                                        v-model="form.password_confirmation"
+                                        type="password"
+                                        class="block mt-1 w-full"
+                                        required
+                                        autocomplete="new-password"
+                                    />
+                                    <InputError class="mt-2" :message="form.errors.password_confirmation" />
+                                </div>
+
+                                <div class="flex items-center justify-end mt-4">
+                                    <PrimaryButton :class="{ 'opacity-25': form.processing }" :disabled="form.processing">
+                                        Reset Password
+                                    </PrimaryButton>
+                                </div>
+                            </form>
+                        </div>
+                    </div>
                 </div>
-            </form>
+            </div>
         </AuthenticationCard>
     </Guest>
 </template>
